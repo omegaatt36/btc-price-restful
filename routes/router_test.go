32c63d2f677ccb867ea0ca5d0403ec21663e60a0
omegaatt36 <omegaatt36@gmail.com>
2020-03-17 22:30:28 +0800

    feat(test file): Add some test file

---
+++ routes/router_test.go
@@ -0,0 +1,41 @@
+package routes
+
+import (
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/appleboy/gofight"
+	"gopkg.in/go-playground/assert.v1"
+)
+
+// GetDefault get a index page for test.
+func testRegister(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Register")
+}
+
+func TestGetDefault(t *testing.T) {
+	r := gofight.New()
+
+	r.GET("/").
+		SetDebug(true). // trun on the debug mode.
+		Run(NewRouter(), func(res gofight.HTTPResponse, rq gofight.HTTPRequest) {
+			assert.Equal(t, "btc-price-restful", res.Body.String())
+			assert.Equal(t, http.StatusOK, res.Code)
+		})
+}
+
+func TestRegister(t *testing.T) {
+	register("GET", "/test",
+		func(w http.ResponseWriter, r *http.Request) {
+			io.WriteString(w, "Register")
+		}, nil)
+	r := gofight.New()
+
+	r.GET("/test").
+		SetDebug(true). // trun on the debug mode.
+		Run(NewRouter(), func(res gofight.HTTPResponse, rq gofight.HTTPRequest) {
+			assert.Equal(t, "Register", res.Body.String())
+			assert.Equal(t, http.StatusOK, res.Code)
+		})
+}
